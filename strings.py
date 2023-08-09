fbbdc47cbf38be87852a9c2f2902141a0ffa10cd
Terry Phan <141813901+ngoclong222@users.noreply.github.com>
2023-08-09 15:41:17 +0700

    Update and rename string.py to strings.py

--- strings.py
+++ strings.py
@@ -7,11 +7,11 @@ print(string_2)
 string_3 = "He said that \"He love me\""  # Use backslash \ before a character to remove meaning
 print(string_3)
 
-age = '26'
-print('You are ' + age)  # Print out a string and a variable including another string
+age_1 = '26'
+print('You are ' + age_1)  # Print out a string and a variable including another string
 
-age = 26
-string_4 = str(age)  # Convert data type from number to string, then connect to another string
+age_2 = 26
+string_4 = str(age_2)  # Convert data type from number to string, then connect to another string
 print("I'm also " + string_4)
 
 height = 162
@@ -19,17 +19,16 @@ print(f'Your height is {height}' + ' cm')
 # Use f"string + {variable}". Curly braces {} can help format a variable to string type
 # We can use f-strings like this: f"""strings""" (That's called multi-line quotation mark)
 
-name = 'Terry'
+name_1 = 'Terry'
 greeting_1 = 'How are you, {}?'
-terry_greeting = greeting_1.format(name)
+terry_greeting = greeting_1.format(name_1)
 # Format things inside the curly braces of a variable by putting another variable into {}
 # If we have multi-curly braces with different values, the program will execute formatting in order
 print(terry_greeting)
 
-name = 'Bonnie'
-greeting_2 = 'How do you do, {name}?'
+greeting_2 = 'How do you do, {name_2}?'
 # Include a variable name into {}, then format the variable greeting_2 manually as following
-bonnie_greeting = greeting_2.format(name='Bonnie')
+bonnie_greeting = greeting_2.format(name_2='Bonnie')
 print(bonnie_greeting)
 
 my_name = 'Long'
