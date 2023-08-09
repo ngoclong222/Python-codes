b8536fa1fedd10b8fb5d933598205ab860c16551
Terry Phan <141813901+ngoclong222@users.noreply.github.com>
2023-08-09 15:01:44 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+print('Hello Mars!', '\n')  # Escape and create a new line
+
+integer_1 = 2 ** 3  # Exponentiation
+print(integer_1)
+
+integer_2 = 2 * 5  # Multiplication
+print(integer_2)
+
+float_1 = 12 / 3  # Division (always return float number)
+print(float_1)
+
+float_2 = 15 // 2  # Floor division (remove decimal part in the result)
+print(float_2)
+
+remainder = 9 % 4  # Modulo division (only return remainder of division, not quotient)
+print(remainder)  # Dividend : Divisor = Quotient
